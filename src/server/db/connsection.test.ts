42afb134be1ab7cabec545eddbd40e1a6de8efc9
MarioGrusz <mario.gruszczynski@gmail.com>
2024-09-23 00:08:53 +0200

    layout of app changed

--- src/server/db/connsection.test.ts
+++ src/server/db/connsection.test.ts
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { describe, test, expect, beforeAll, afterAll } from "vitest";
-import { Client } from "pg";
-import { checkDatabaseConnection } from "./connection";
+// import { describe, test, expect, beforeAll, afterAll } from "vitest";
+// import { Client } from "pg";
+// import { checkDatabaseConnection } from "./connection";
 
-describe("connection test", () => {
-  let client: Client;
+// describe("connection test", () => {
+//   let client: Client;
 
-  test("should have a connection to test database", async () => {
-    const connect = await checkDatabaseConnection();
-  });
-});
+//   test("should have a connection to test database", async () => {
+//     const connect = await checkDatabaseConnection();
+//   });
+// });
--- src/server/db/test-utils/helper.ts
+++ src/server/db/test-utils/helper.ts
@@ -1,69 +1,69 @@
-import { Client } from "pg";
-import { env } from "../../../env";
+// import { Client } from "pg";
+// import { env } from "../../../env";
 
-const config = {
-  host: env.PG_TEST_HOST,
-  port: Number(env.PG_PORT),
-  user: env.PG_TEST_USER,
-  password: env.PG_PASSWORD,
-  database: env.PG_TEST_DATABASE,
-};
+// const config = {
+//   host: env.PG_TEST_HOST,
+//   port: Number(env.PG_PORT),
+//   user: env.PG_TEST_USER,
+//   password: env.PG_PASSWORD,
+//   database: env.PG_TEST_DATABASE,
+// };
 
-console.log("CONFIG", config);
+// console.log("CONFIG", config);
 
-// console.log({
-//   PG_TEST_USER: process.env.PG_TEST_USER,
-//   PG_TEST_HOST: process.env.PG_TEST_HOST,
-//   PG_PASSWORD: process.env.PG_PASSWORD,
-//   PG_TEST_DATABASE: process.env.PG_TEST_DATABASE,
-//   PG_PORT: process.env.PG_PORT,
-// });
+// // console.log({
+// //   PG_TEST_USER: process.env.PG_TEST_USER,
+// //   PG_TEST_HOST: process.env.PG_TEST_HOST,
+// //   PG_PASSWORD: process.env.PG_PASSWORD,
+// //   PG_TEST_DATABASE: process.env.PG_TEST_DATABASE,
+// //   PG_PORT: process.env.PG_PORT,
+// // });
 
-const runWithDefaultDatabaseConnection = async (
-  fn: (client: Client) => Promise<void>
-): Promise<void> => {
-  const client = new Client(config);
-  await client.connect();
-  await fn(client);
-  await client.end();
-};
+// const runWithDefaultDatabaseConnection = async (
+//   fn: (client: Client) => Promise<void>
+// ): Promise<void> => {
+//   const client = new Client(config);
+//   await client.connect();
+//   await fn(client);
+//   await client.end();
+// };
 
-/**
- * Creates a new database using template0 and
- * returns a `Client` connected to the newly created database.
- *
- * @returns `Client`
- */
-export const createTestDatabase = async (): Promise<Client> => {
-  if (process.env.VITEST === undefined) {
-    throw new Error("Function was not executed with Vitest.");
-  }
+// /**
+//  * Creates a new database using template0 and
+//  * returns a `Client` connected to the newly created database.
+//  *
+//  * @returns `Client`
+//  */
+// export const createTestDatabase = async (): Promise<Client> => {
+//   if (process.env.VITEST === undefined) {
+//     throw new Error("Function was not executed with Vitest.");
+//   }
 
-  const database = `vitest-db-${crypto.randomUUID()}`;
-  await runWithDefaultDatabaseConnection(async (client) => {
-    await client.query(`CREATE DATABASE "${database}" TEMPLATE template0;`);
-  });
-  const client = new Client({ ...config, database });
-  await client.connect();
-  return client;
-};
+//   const database = `vitest-db-${crypto.randomUUID()}`;
+//   await runWithDefaultDatabaseConnection(async (client) => {
+//     await client.query(`CREATE DATABASE "${database}" TEMPLATE template0;`);
+//   });
+//   const client = new Client({ ...config, database });
+//   await client.connect();
+//   return client;
+// };
 
-/**
- * Closes the connection and drops the database.
- */
+// /**
+//  * Closes the connection and drops the database.
+//  */
 
-export const teardownTestDatabase = async (client: Client): Promise<void> => {
-  if (process.env.VITEST === undefined) {
-    throw new Error("Function was not executed with Vitest.");
-  }
+// export const teardownTestDatabase = async (client: Client): Promise<void> => {
+//   if (process.env.VITEST === undefined) {
+//     throw new Error("Function was not executed with Vitest.");
+//   }
 
-  const database = await client
-    .query<{ current_database: string }>("SELECT current_database();")
-    .then(({ rows }) => rows[0]?.current_database);
+//   const database = await client
+//     .query<{ current_database: string }>("SELECT current_database();")
+//     .then(({ rows }) => rows[0]?.current_database);
 
-  await client.end();
+//   await client.end();
 
-  await runWithDefaultDatabaseConnection(async (client) => {
-    await client.query(`DROP DATABASE "${database}";`);
-  });
-};
+//   await runWithDefaultDatabaseConnection(async (client) => {
+//     await client.query(`DROP DATABASE "${database}";`);
+//   });
+// };
